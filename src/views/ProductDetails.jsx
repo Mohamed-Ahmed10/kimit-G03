4a06796a7cf717127e69db88743d1a7f0871a404
Mohamed Ahmed <50152939+Mohamed-Ahmed10@users.noreply.github.com>
2023-08-16 22:33:06 +0300

    Final project session 2

--- src/views/ProductDetails.jsx
+++ src/views/ProductDetails.jsx
@@ -1,19 +1,16 @@
 import { useEffect, useState } from "react";
 import { useParams } from "react-router-dom"
-import Carousel from 'react-bootstrap/Carousel';
 
 
 export default function ProductDetails() {
     const params = useParams();
 
     let [product, setProduct] = useState({});
-    let [productImages, setProductImages] = useState([])
 
     function getSingleProduct() {
         fetch("https://dummyjson.com/products/" + params.productId)
             .then(json => json.json())
             .then(res => setProduct(res))
-            .then(setProductImages(product.images))
     }
 
     useEffect(() => {
